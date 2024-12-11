f3db18a5f69a0d82d294e5a1a140fc1729fbc963
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-11 23:44:59 +0700

    add api for channels

---
+++ src/app/services/channelsApi.ts
@@ -0,0 +1,42 @@
+import { Channels, Messages } from "../types";
+import { api } from "./api";
+
+export const channelsApi = api.injectEndpoints({
+	endpoints: (builder) => ({
+		createChannel: builder.mutation<Channels, {name: string}>({
+			query: (channelData) => ({
+				url: "/create",
+				method: "POST",
+				body: channelData
+			})
+		}),
+		addUser: builder.mutation<{channelId: string, userId: string}, {channelId: string, userId: string}>({
+			query: (channelData) => ({
+				url: '/add',
+				method: "PUT",
+				body: channelData
+			})
+		}),
+		joinChannel: builder.mutation<Channels, {channelId: string}>({
+			query: (joinData) => ({
+				url: "/join",
+				method: "POST",
+				body: joinData
+			})
+		}),
+		deleteUser: builder.mutation<{channelId: string; userId: string}, {channelId: string; userId: string}>({
+			query: (deleteData) => ({
+				url: '/delete',
+				method: "DELETE",
+				body: deleteData
+			})
+		}),
+		sendMessage: builder.mutation<Messages, {channelId: string; text: string}>({
+			query: (messageData) => ({
+				url: '/send-message',
+				method: "POST",
+				body: messageData
+			})
+		})
+	})
+})
