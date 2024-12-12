55f54be6a721dc66c2050b7c53666033d2871470
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-12 15:12:22 +0700

    a component for chat navigation

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import styled from './index.module.css';
-import { Input } from '@nextui-org/react';
+import { ChatItem } from '../ChatItem';
+import { Search } from '../Search';
+import { useAppSelector } from '../../hooks';
 
 export const Nav = () => {
+	const channels = useAppSelector((state) => state.auth.user?.Channels);
+  console.log(channels);
   return (
 	  <div className={styled['chat-nav']}>
-      <div className={styled['chat-head']}>
-        <Input type="text" labelPlacement="outside" placeholder="Search" />
+      <Search />
+      <div className={styled['chat-list']}>
+        <ChatItem name='Chatgram' time='19:53' lastMessage='dlpaoid adas fdsfg' />
+        {channels ? channels.map((channel) => (
+          <ChatItem 
+            key={channel.id} // Убедитесь, что у каждого чата есть уникальный идентификатор
+            name={channel.name}
+            time='19:44'
+            lastMessage='test'
+          />
+        )) : false}
       </div>
     </div>
   )
