5587186e388e9a3a3c567a37bcffc086412b0e1b
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-13 23:53:28 +0700

    message components

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,31 @@
+import { Avatar, Card } from '@nextui-org/react';
+import { User } from '../../app/types';
+
+type Props = {
+	text: string;
+	senderId: string;
+	isMe: boolean;
+	createdAt: Date;
+	name: string;
+	id: string;
+}
+
+export const Message = ({text, senderId, isMe, createdAt, id, name}: Props) => {
+	if (!name) return <div>Error: User name is missing</div>;
+	return (
+		<div className={`flex w-[100%] mb-2`} key={id}>
+			{isMe ? (
+				<div className="w-[100%] flex gap-2 justify-end">
+					<Card className={`pt-2 pb-2 pr-8 pl-4 inline-block rounded-small`}>{text}</Card>
+					<Avatar name={name} />
+				</div>
+			) : (
+				<div className="w-[100%] flex gap-2">
+					<Avatar name={name} />
+					<Card className={`pt-2 pb-2 pr-8 pl-4 inline-block rounded-small`}>{text}</Card>
+				</div>
+			)}
+			
+		</div>
+	)
+}
