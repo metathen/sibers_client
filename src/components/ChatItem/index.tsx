b62d8fb2e8a5c8ed5e7e9bd97117498ccf4acac1
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-13 23:41:56 +0700

    the chat-nav item components

--- src/components/ChatItem/index.tsx
+++ src/components/ChatItem/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from '../Nav/index.module.css';
-import { Image } from '@nextui-org/react';
+import { Avatar } from '@nextui-org/react';
 
 type Props = {
 	name: string;
@@ -12,19 +12,14 @@ export const ChatItem = ({name, time, lastMessage}: Props) => {
   return (
 	<div className={style['chat-item'] + ' cursor-pointer'}>
 		<div className={style['chat-logo']}>
-			<Image
-				alt="NextUI hero Image"
-				src="https://nextui.org/images/hero-card-complete.jpeg"
-				width={48}
-				height={48}
-			/>
+			<Avatar name={name} />
 		</div>
 		<div className={style['chat-name']}>
 			<p className='t-smb'>{name}</p>
 			<span>{lastMessage}</span>
 		</div>
 		<div className={style['chat-info']}>
-			<span>{time}</span>
+			<span></span>
 		</div>
 	</div>
   )
