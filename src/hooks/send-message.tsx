ec299adab12058bb096a5375fd74337f6fcd0fbf
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-19 18:10:00 +0700

    add logs for check connection socket io

--- src/hooks/send-message.tsx
+++ src/hooks/send-message.tsx
@@ -1,25 +1,28 @@
 import { useEffect } from 'react';
 import { io, Socket } from 'socket.io-client';
-
-const SOCKET_URL = 'http://localhost:3080';
+import { BASE_URL } from '../const';
 
 type UseSocketProps = {
   channelId: string;
-  onReceiveMessage: (message: any) => void; //Callback function for handling messages
+  onReceiveMessage: (message: any) => void; //сallback function for handling messages
+  onUserJoined?: (userCount: number) => void;
 }
 
-const useSocket = ({ channelId, onReceiveMessage }: UseSocketProps) => {
+const useSocket = ({ channelId, onReceiveMessage}: UseSocketProps) => {
   useEffect(() => {
     if (!channelId) return;
 
-    const socket: Socket = io(SOCKET_URL);
+    const socket: Socket = io(BASE_URL);
     console.log('Socket connected:', socket.connected); // check connection
-
     // join
     socket.emit('joinChannel', channelId);
 
-    // Handling the reception of a new message
-    socket.on('receiveMessage', onReceiveMessage);
+    socket.on("connect", () => console.log("Socket connected:", socket.id));
+    socket.on("connect_error", (err) => console.error("Connection error:", err));
+    socket.on("disconnect", () => console.log("Socket disconnected"));
+    socket.connect();
+
+    socket.on('receiveMessage', (message) => onReceiveMessage(message));
 
     return () => {
       socket.disconnect();
