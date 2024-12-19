2f2ad4481026e80798b66cbaea4791b1c3fed835
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-19 18:03:09 +0700

    add state for channel function

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Nav } from '../Nav'
 import styled from './index.module.css'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectIsAuth, selectUser } from '../../features/user-slice'
 import { useNavigate } from 'react-router-dom'
 import { ChatContent } from '../ChatContent'
 import { AddChannel } from '../AddChannel'
+import { Channels } from '../../app/types'
+import { useAppSelector } from '../../hooks'
 
 export const Layout = () => {
 	const  isAuthenticated = useSelector(selectIsAuth);
-	const user = useSelector(selectUser);
 	const navigate = useNavigate();
+	const [channels, setChannels] = useState<Channels[]>([]);
+
+	const channelList = useAppSelector((state) => state.auth.user?.channels);
+
+	useEffect(() => {
+		if (channelList) {
+			setChannels(channelList);
+		}
+	}, [channelList]);
+
+	const handleAddChannel = (newChannel: Channels) => {
+		setChannels((prevChannels) => [...prevChannels, newChannel]);
+	};
+
+	useEffect(() => {
+		if (channelList) {
+		  setChannels(channelList);
+		}
+	}, [channelList]);
+
+  	//add new channel
+	const addChannel = (newChannel: Channels) => {
+		setChannels((prevChannels) => (Array.isArray(prevChannels) ? [...prevChannels, newChannel] : [newChannel]));
+	};
+
+	const handleJoinChannel = (joinedChannel: Channels) => { //for sign new channel
+		setChannels((prevChannels) => (Array.isArray(prevChannels) ? [...prevChannels, joinedChannel] : [joinedChannel]));
+	};
 
 	useEffect(() => {
 		if(!isAuthenticated) navigate('/auth');
@@ -19,9 +48,9 @@ export const Layout = () => {
 	return (
 		<div className={styled.box}>
 			<div className={styled.chat}>
-				<Nav />
+				<Nav channels={channels} />
 				<ChatContent />
-				<AddChannel />
+				<AddChannel onAddChannel={addChannel} />
 			</div>
 		</div>
 	)
