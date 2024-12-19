9aaafec13c266e9bfb8d9762e41a3a4f30462782
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-19 18:07:09 +0700

    add handle sign

--- src/components/ChatNavbar/index.tsx
+++ src/components/ChatNavbar/index.tsx
@@ -15,12 +15,15 @@ import { useDeleteUserMutation, useGetChannelByIdQuery, useJoinChannelMutation }
 import { useGetUserByIdMutation } from "../../app/services/userApi";
 import { User } from "../../app/types";
 import { errorField } from "../../utils/error-field";
+import { io, Socket } from "socket.io-client";
+import { BASE_URL } from "../../const";
 
 export const ChatNavbar = () => {
 	const { id } = useParams<{ id: string }>();
 
 	if(!id) return;
 
+	const socket: Socket = io(BASE_URL);
 	//save users data
 	const [userList, setUserList] = useState<User[]>([]);
 
@@ -38,18 +41,10 @@ export const ChatNavbar = () => {
 
 	//get channel data
 	const { data: channel, isLoading: isChannelLoading, error: channelError } = useGetChannelByIdQuery(id || "");
-
-	useEffect(() => {
-		if (channel?.members) {
-		  setUserList(channel.members);
-		}
-	}, [channel?.members]);
   
 	const [isUserInChannel, setIsUserInChannel] = useState<boolean>(false);
 
 	const [joinChannel, { isLoading }] = useJoinChannelMutation() //for join in the channel
-
-	const [getUserById, { error }] = useGetUserByIdMutation();
 
 	const handleDelete = async (userId: string) => { //function delete the user
 		try {
@@ -59,11 +54,23 @@ export const ChatNavbar = () => {
 		  	}
 		} catch (err) {
 		  	console.error('Error deleting user:', err);
-			if(errorField(errorLog)) {
-				setError(errorLog.data.error);
-			}
+			if(errorField(errorLog)) setError(errorLog.data.error);
 		}
 	};
+
+	const handleSign = async () => { //function join
+		try {
+			const response = await joinChannel({ channelId: id }).unwrap(); //start endpoint
+
+			//server notification about a new member socket
+			console.log(response)
+			// Update UI
+			setIsUserInChannel(true);
+		} catch (error) {
+		  	console.error("Error joining channel:", error);
+		}
+	};
+
   
 	//сheck if the user is a member of this chat
 	useEffect(() => {
@@ -73,23 +80,18 @@ export const ChatNavbar = () => {
 			setIsUserInChannel(isMember); //setting the state if the user is a member of this chat
 		}
 	}, [id, user]);
-
-	const handleSign = async () => { //function join
-		try {
-			const response = await joinChannel({ channelId: id }).unwrap(); //start endpoint
-			// Update UI
-			setIsUserInChannel(true);
-		} catch (error) {
-		  	console.error("Error joining channel:", error);
+	useEffect(() => {
+		if (channel?.members) {
+		  setUserList(channel.members);
 		}
-	};
+	}, [channel?.members]);
 	return (
 		<Navbar isBordered>
 			<NavbarBrand className="flex gap-2 align-center cursor-pointer" onClick={onOpen}>
 				<Avatar name={channel?.name} />
 				<div className={styled['head-box']}>
 					<p className="font-semibold">{channel?.name}</p>
-					<span>{channel?.members.length} users</span>
+					<span>{userList.length} users</span>
 				</div>
 			</NavbarBrand>
 			<Modal
@@ -128,7 +130,7 @@ export const ChatNavbar = () => {
 												color="danger"
 												variant="bordered"
 											>
-												Delete
+												{isLoading ? "Deleting..." : "Delete"}
 											</Button>
 										)}
 									</div>
